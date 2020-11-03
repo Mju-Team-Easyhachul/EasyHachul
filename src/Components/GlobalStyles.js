bca3a538f081f425580c83f21c0a176dbbbb212a
noah0316 <noa316@naver.com>
2020-11-03 23:34:08 +0900

    Change Font  - GlobalStyles.js : Change font GmarketSansBold to GmarketSansMedium

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -4,14 +4,14 @@ import reset from "styled-reset";
 const globalStyles = createGlobalStyle`
     ${reset};
     @font-face {
-        font-family: 'GmarketSansBold';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff') format('woff');
+        font-family: 'GmarketSansMedium';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
     }
     html, body {
         height: 100%;
     }
     #root {
-        font-family: 'GmarketSansBold';
+        font-family: 'GmarketSansMedium';
         font-weight: normal;
         font-style: normal;
         height: 100%;
