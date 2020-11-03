a6b120906ab92e896b7a95c234f6b8ea7657a7cc
noah0316 <noa316@naver.com>
2020-11-03 23:27:54 +0900

    Add Font - GlobalStyles.js : Add font Gmarket Sans

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -3,18 +3,20 @@ import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
     ${reset};
-    a{
-        text-decoration: none;
-        color:inherit;
+    @font-face {
+        font-family: 'GmarketSansBold';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff') format('woff');
     }
-
-    *{
-       
+    html, body {
+        height: 100%;
     }
-
-    body{
-        
+    #root {
+        font-family: 'GmarketSansBold';
+        font-weight: normal;
+        font-style: normal;
+        height: 100%;
     }
+    
 
 
 `;
