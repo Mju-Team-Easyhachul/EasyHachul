6a2200d6f1f15ab6e8f91347ce5697484965a7a5
noah0316 <noa316@naver.com>
2020-11-16 21:57:12 +0900

    Implement Report Page - .eslintrc.json : Defined eslint rules - package.json : Add dependencies(react-bootstrap), devdependencies (eslint, eslint-config-prettier, eslint-plugin-react, prettier) - index.html : Add bootstrap CDN - ReportContainer.js : Handled Report Page event - ReportPresenter.js : Show Report Page

--- src/Routes/Report/ReportContainer.js
+++ src/Routes/Report/ReportContainer.js
@@ -1,8 +1,22 @@
-import React from "react";
-import ReportPresenter from "./ReportPresenter";
+import React from 'react';
+import ReportPresenter from './ReportPresenter';
 
 export default class extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      reportText: ['마스크<br/>미착용자<br/>민원신고', '임산부석<br/>민원신고', '취객난동<br/>민원신고'],
+    };
+  }
+
+  handleReportButton(e) {
+    console.log(e);
+  }
+
   render() {
-    return <ReportPresenter />;
+    console.log('true');
+    const { reportText } = this.state;
+    return <ReportPresenter reportText={reportText} handleReportButton={this.handleReportButton} />;
   }
 }
