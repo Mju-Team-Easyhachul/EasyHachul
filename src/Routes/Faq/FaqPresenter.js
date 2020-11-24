f25719bb715fb4c21813690948b6c310e3fdf7ba
parkchowon <parkcandle@gmail.com>
2020-11-24 15:42:56 +0900

    home and FaQ presenter

--- src/Routes/Faq/FaqPresenter.js
+++ src/Routes/Faq/FaqPresenter.js
@@ -1,11 +1,93 @@
 import React from "react";
 import styled from "styled-components";
 import Helmet from "react-helmet";
+import Header from "../../Components/Header";
+
+const FaqBoardContainer = styled.div`
+  height:1008px;
+  width:803px;
+  padding-top: 0.1%;
+  position: absolute;
+  left: 28%;
+`;
+
+const FaqBoard = styled.div`
+  height:308px;
+  width:803px;
+  margin-bottom: 4.76%;
+  padding: 5.3% 10.9% 5.8% 3.9%;
+  border-radius: 50px;
+  background-color:#B6E6DB;
+`;
+
+const FaqText1 = styled.div`
+  font-size: 21px;
+  font-weight: bold;
+  text-align: left;
+  margin-bottom: 2%;
+  line-height: 24px;
+`;
+
+const FaqText2 = styled.div ` 
+  font-size: 14px;
+  font-weight: 500;
+  margin-bottom: 2%;
+`;
+
+const FaqText3 = styled.div ` 
+  width: 683px;
+  height: 182px;
+  font-size: 15px;
+  line-height: 24px;
+`;
+
+const FaqTextline = styled.div `
+  margin-bottom: 3%;
+`;
 
 const FaqPresenter = () => (
+  <>
+
   <Helmet>
     <title>FAQ | EasyHachul</title>
   </Helmet>
+  <Header></Header>
+  
+  <FaqBoardContainer>
+    <FaqBoard>
+      <FaqText1>반려 동물과 대중교통 이용, 이것만 알아두세요 !</FaqText1>
+      <FaqText2>Easy하철 알림</FaqText2>
+        <FaqText3>
+          <FaqTextline>이번 포스팅에서는 반려동물과 함께 대중교통을 이용할 때 알아두면 좋을 이용규정을 소개합니다.</FaqTextline>
+          <FaqTextline>★ 이동장비(케이지)에 넣는 등 안전조치를 취한 후 탑승하기</FaqTextline>
+          <FaqTextline>(「도시철도법」 제32조, 「광역철도 여객운송 약관」 제31조제2호, 제32조제1항, 「서울교통공사 여객운송약관」 제34조제1항제4호).</FaqTextline>
+        </FaqText3>
+      </FaqBoard>
+      <FaqBoard>
+        <FaqText1>대중교통 부정승차 시, 이렇게 처벌받아요!</FaqText1>
+        <FaqText2>Easy하철 알림</FaqText2>
+        <FaqText3>
+          <FaqTextline>
+            <p>지하철과 버스는 정해진 운임요금과 승차방법으로 편리함을 제공하는 교통 수단입니다.</p>
+            ★ 지하철에 부정승차 할 경우 승차 구간에 해당하는 운임 외에 30배의 범위에서 부가운임을 징수할 수 있습니다
+            (「철도사업법」 제10조제1항) 부가운임을 납부하지 않을 경우 형사고발 될 수 있습니다.
+          </FaqTextline>
+          <FaqTextline>
+            ★ 시내버스 부정승차로 적발되면 그 운임 또는 부족하게 지불한 운임과 그 운임의 30배를 부가금으로 지불해야 하며, 부가금을 현금으로 납부해야 합니다[「시내버스 운송사업 약관」(서울시버스정책과-15198호, 2020. 6. 1. 발령, 2020. 6. 11. 시행) 제13조의2].  부가운임을 납부하지 않을 경우 형사고발 될 수 있습니다.            
+          </FaqTextline>
+        </FaqText3>
+      </FaqBoard>
+      <FaqBoard>
+        <FaqText1>지하철에 물건을 두고 내렸을 때, 이렇게 찾으세요 !</FaqText1>
+        <FaqText2>Easy하철 알림</FaqText2>
+        <FaqText3>
+          <FaqTextline>대중교통을 타고 가다가 목적지에 황급히 내리거나, 다른곳에 집중하다 내려서 물건을 깜빡 두고 내리는 경험 있으실겁니다. 선물을 담은 쇼핑백, 비가왔던 날의 우산, 바지 뒷주머니의 지갑 등 소중한 물건을 두고 내린 순간을 알게되면 굉장히 당혹스러움을 느끼며 어떻게 찾아야할지 우왕좌왕하게 됩니다.</FaqTextline>
+          <FaqTextline>대중교통 이용 중 물건을 두고 내렸을 때 알아두면 좋은 경찰청 유실물 통합 포털 LOST112 를 소개합니다 !</FaqTextline>
+          <FaqTextline>★ (경찰청 유실물 통합포털 시스템 : www.lost112.go.kr)</FaqTextline>
+        </FaqText3>
+      </FaqBoard>
+    </FaqBoardContainer>
+  </>
 );
 
 export default FaqPresenter;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,11 +1,259 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import Helmet from "react-helmet";
+import Logo from "../../Components/Logo";
+import LeftIcon from "../../assets/Quoteleft.png";
+import RightIcon from "../../assets/Quoteright.png";
+import ArrowIcon from "../../assets/Forwardarrow.png";
+import BackgroundImage from "../../assets/homebackground.png";
+
+const AllContainer = styled.div `
+
+`;
+//EASY하철 전체
+const Titlecontainer = styled.div `
+  padding: 9% 38% 0% 38%;
+  display: flex;
+  justify-content: center;
+`;
+
+//왼쪽따옴표 아이콘
+const Icon1 = styled.img.attrs({
+  src: LeftIcon,
+})`
+  width: 44px;
+  height: 44px
+`;
+
+//오른쪽따옴표 아이콘
+const Icon2 = styled.img.attrs({
+  src: RightIcon,
+})`
+  width: 44px;
+  height: 44px;
+`;
+
+//로고 이미지
+const Logocontainer = styled.div `
+  margin-left: 2.3%;
+  width: 67px;
+  height: 67px;
+  odjext-fit:contain;
+`;
+
+//EASY하철 텍스트
+const Title = styled.div `
+  width: 272px;
+  height: 63px;
+  font-size: 55px;
+  line-height: 1.15;
+  text-align: left;
+  color: black;
+  margin-left : 2.3%;
+  margin-right : 2.3%;
+  white-space:nowrap;
+`;
+
+//부가설명 문구 전체
+const TextContainer = styled.div `
+  width: 538px;
+  height: 80px;
+  display : block;
+  margin : 6% auto;
+`;
+
+//부가설명 텍스트
+const Text = styled.div `
+  text-align : center;
+  line-height: 1.14;
+  font-weight: 500;
+  -webkit-text-stroke: 1px #000000;
+  font-size: 34px;
+  color: black;
+
+`;
+
+//버튼전체
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+//버튼모양
+const Button = styled.div`
+  width: 231px;
+  height: 231px;
+  border-radius: 50%;
+  border: none;
+  background-color: #84e0cb;
+  margin: 4% 8.9% 0 0;
+`;
+
+//마지막버튼
+const Button1 = styled.div`
+  width: 231px;
+  height: 231px;
+  border-radius: 50%;
+  border: none;
+  background-color: #84e0cb;
+  margin-top : 4%;
+`;
+
+//버튼 텍스트
+const ButtonText = styled.div`
+  width: 135px;
+  height: 40px;
+  font-size: 35px;
+  color: black;
+  margin: 36.3% 21.6% 8.2% 19.9%;
+  text-align:center;
+`;
+
+//버튼 텍스트(노선로 다운로드)
+const ButtonText1 = styled.div`
+  width: 116px;
+  height: 70px;
+  font-size: 30px;
+  -webkit-text-stroke: 1px #000000;
+  color: black;
+  margin: 30.7% 24.6% -7.8% 25.1%;
+  text-align:center;
+`;
+
+//화살표이미지박스
+const ArrowContainer = styled.div`
+  width: 21.6%;
+  height: 21.6%;
+  margin: 8.2% 36.7% auto auto;
+`;
+
+//화살표 이미지
+const Icon3 = styled.img.attrs({
+  src: ArrowIcon,
+})`
+  width: 100%;
+  height: 100%;
+`;
+
+//배경화면
+const BackImage = styled.div`
+
+  background-image: url(${BackgroundImage});
+  background-size: cover;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right:0;
+  bottom:0;
+`;
+
+const DownloadButtonContainer = styled.div `
+  display: flex;
+  justify-content: center;
+`;
+
+const DownloadButton = styled.div `
+width: 327px;
+height: 64px;
+border-radius: 4px;
+margin-top: 4%;
+background-color: #7ad9c4;
+display : flex;
+`;
+
+const DownloadButtonText = styled.div `
+width: 196px;
+height: 25px;
+font-size: 29px;
+text-align : center;
+margin: auto 0 auto 18%;
+`;
+
+const DownloadArrow = styled.div `
+  width: 24px;
+  height: 24px;
+  margin: auto 9.1% auto 5.5%;
+`;
 
 const HomePresenter = () => (
-  <Helmet>
-    <title>Home | EasyHachul</title>
-  </Helmet>
+
+  <>
+
+    <Helmet>
+      <title>Home | EasyHachul</title>
+    </Helmet>
+
+    <BackImage>
+
+ 
+      <AllContainer>
+      <Titlecontainer>
+        <Icon1></Icon1>
+        <Logocontainer>
+          <Logo width="67px" height="67px" />
+        </Logocontainer>
+        <Title>EASY하철</Title>
+        <Icon2></Icon2>
+      </Titlecontainer>
+
+      <TextContainer>
+        <Text>
+          가장 쉽고 빠른 지하철 길찾기,
+          EASY하철에서 바로 확인해보세요!
+      </Text>
+      </TextContainer>
+
+      <ButtonContainer>
+
+        <Button>
+          <Link to="Search" style={{ textDecoration: "none" }}>
+            <ButtonText>경로검색</ButtonText>
+          </Link>
+          <ArrowContainer>
+            <Icon3></Icon3>
+          </ArrowContainer>
+        </Button>
+
+        <Button>
+          <Link to="Download" style={{ textDecoration: "none" }}>
+            <ButtonText1>노선로 다운로드</ButtonText1>
+          </Link>
+          <ArrowContainer>
+            <Icon3></Icon3>
+          </ArrowContainer>
+        </Button>
+
+        <Button>
+          <Link to="Report" style={{ textDecoration: "none" }}>
+            <ButtonText>민원신고</ButtonText>
+          </Link>
+          <ArrowContainer>
+            <Icon3></Icon3>
+          </ArrowContainer>
+        </Button>
+
+        <Button1>
+          <Link to="Faq" style={{ textDecoration: "none" }}>
+            <ButtonText>FAQ</ButtonText>
+          </Link>
+          <ArrowContainer>
+            <Icon3></Icon3>
+          </ArrowContainer>
+        </Button1>
+
+      </ButtonContainer>
+      <DownloadButtonContainer>
+        <DownloadButton>
+        <DownloadButtonText>노선도보러가기</DownloadButtonText>
+        <DownloadArrow><Icon3></Icon3></DownloadArrow>
+        </DownloadButton>
+        </DownloadButtonContainer>
+    </AllContainer>
+    </BackImage>
+
+  </>
 );
+
 
 export default HomePresenter;
