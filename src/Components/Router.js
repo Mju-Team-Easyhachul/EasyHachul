b94e44742677ede2dc209c00f0664e0cc51a6d11
jinsu-l <wlstn8738@naver.com>
2020-12-06 05:27:10 +0900

    feat: add RouteMap & install d3.js,react-d3-graph & modify Router Detail url

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,27 +1,24 @@
-import React from "react";
-import {
-  HashRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from "react-router-dom";
-import Detail from "../Routes/Detail";
-import Download from "../Routes/Download";
-import Faq from "../Routes/Faq";
-import Home from "../Routes/Home";
-import Report from "../Routes/Report";
-import Search from "../Routes/Search";
+import React from 'react';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Detail from '../Routes/Detail';
+import Download from '../Routes/Download';
+import Faq from '../Routes/Faq';
+import Home from '../Routes/Home';
+import Report from '../Routes/Report';
+import Search from '../Routes/Search';
+import RouteMap from '../Routes/RouteMap';
 
 export default () => (
   <Router>
     <>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/Detail" component={Detail} />
+        <Route path="/Detail:nodeId" component={Detail} />
         <Route path="/Download" component={Download} />
         <Route path="/Faq" component={Faq} />
         <Route path="/Report" component={Report} />
         <Route path="/Search" component={Search} />
+        <Route path="/RouteMap" component={RouteMap} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
